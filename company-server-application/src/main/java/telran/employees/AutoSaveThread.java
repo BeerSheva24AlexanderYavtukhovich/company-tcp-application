818dd0bab13fd7d152c21380e44ed8d3613cfa5d
Alexander Yavtuhovich <yavtik@gmail.com>
2024-12-09 13:15:32 +0200

    gracefull shutdown, dos attack test

--- company-server-application/src/main/java/telran/employees/AutoSaveThread.java
+++ company-server-application/src/main/java/telran/employees/AutoSaveThread.java
@@ -6,6 +6,7 @@ public class AutoSaveThread extends Thread {
     private final Persistable persistable;
     private final String filePath;
     private final int saveInterval;
+    private boolean running = true;
 
     public AutoSaveThread(Persistable persistable) {
         this.persistable = persistable;
@@ -13,10 +14,12 @@ public class AutoSaveThread extends Thread {
         this.saveInterval = ServerConfig.SAVE_INTERVAL;
         setDaemon(true);
     }
-
+    public void stopThread() {
+        running = false;
+    }
     @Override
     public void run() {
-        while (true) {
+        while (running) {
             try {
                 persistable.saveToFile(filePath);
                 Thread.sleep(saveInterval);
--- company-server-application/src/main/java/telran/employees/Main.java
+++ company-server-application/src/main/java/telran/employees/Main.java
@@ -2,17 +2,65 @@ package telran.employees;
 
 import telran.io.Persistable;
 import telran.net.TcpServer;
+import telran.view.InputOutput;
+import telran.view.Item;
+import telran.view.Menu;
+import telran.view.StandardInputOutput;
 
 public class Main {
+    private static TcpServer server;
+    private static Thread serverThread;
+    private static AutoSaveThread autoSaveThread;
+
     public static void main(String[] args) {
         Company company = new CompanyImpl();
-        TcpServer server = new TcpServer(new CompanyProtocol(company), ServerConfig.PORT);
+        server = new TcpServer(new CompanyProtocol(company), ServerConfig.PORT);
+
         if (company instanceof Persistable persistable) {
             persistable.restoreFromFile(ServerConfig.DATA_FILE);
-            AutoSaveThread autoSaveThread = new AutoSaveThread(persistable);
+            autoSaveThread = new AutoSaveThread(persistable);
             autoSaveThread.start();
             Runtime.getRuntime().addShutdownHook(new Thread(() -> persistable.saveToFile(ServerConfig.DATA_FILE)));
         }
-        server.run();
+
+        Item[] items = {
+                Item.of("Shutdown", Main::shutdown)
+        };
+        Menu menu = new Menu("Company TCP Server", items);
+
+        serverThread = new Thread(() -> {
+            try {
+                server.run();
+            } catch (Exception e) {
+                System.err.println("Error while running the server: " + e.getMessage());
+            }
+        }, "TcpServerThread");
+        serverThread.start();
+        waitWhileServerStarted();
+        menu.perform(new StandardInputOutput());
+    }
+
+    private static void waitWhileServerStarted() {
+        while (!server.isReady()) {
+            try {
+                Thread.sleep(100);
+            } catch (InterruptedException e) {
+            }
+        }
+    }
+
+    private static void shutdown(InputOutput io) {
+        try {
+            System.out.println("Shutting down the server.");
+            autoSaveThread.stopThread();
+            autoSaveThread.join();
+            server.shutdown();
+            serverThread.join();
+            System.out.println("Server stopped gracefully.");
+        } catch (InterruptedException e) {
+        } finally {
+            System.out.println("Bye.");
+            System.exit(0);
+        }
     }
 }
